f77d59889039b1d0839120fc1a99d1e690ad60ec
mataes2007 <mataes2007@77a0cf66-ed60-b203-0a24-059baa6dd734>
2012-12-08 18:57:45 +0000

    update

--- bin10/my_pascal32.bat
+++ bin10/my_pascal32.bat
@@ -3,17 +3,17 @@ rem @echo off
 pushd ..\plugins
 
 pushd Actman
-call make.bat fpc
+call make.bat fpc 10
 popd
 
 pushd ImportTXT
-call make.bat fpc
+call make.bat fpc 10
 popd
 
 pushd Dbx_mmap_SA\Cryptors\Athena
-call make.bat fpc
+call make.bat fpc 10
 popd
 
 pushd mRadio
-call make.bat fpc
+call make.bat fpc 10
 popd
